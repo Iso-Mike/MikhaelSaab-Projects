740919e4fb90a4b64e6dde8032d942570224c93e
Mikhael Saab <83307112+Iso-Mike@users.noreply.github.com>
2023-12-26 17:14:52 -0500

    Update Fridge.java

--- Assignment4/src/Fridge.java
+++ Assignment4/src/Fridge.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 public class Fridge extends Appliance {
     private boolean hasFreezer;
 
