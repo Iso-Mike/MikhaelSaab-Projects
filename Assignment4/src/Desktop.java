35fa9dc76e747230766d4387e906c4e28ca3d0e7
Mikhael Saab <83307112+Iso-Mike@users.noreply.github.com>
2023-12-26 17:14:15 -0500

    Update Desktop.java

--- Assignment4/src/Desktop.java
+++ Assignment4/src/Desktop.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 public class Desktop extends Computer {
     private String towerProfile;
 
